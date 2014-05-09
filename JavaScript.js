6d07d7fd738b1caa0fdb9b7bf76357f25134f4c9
Mivexil <maciejas_jmj@o2.pl>
2014-05-09 19:39:44 +0200

    JS refactoring.
    I have a truly marvellous commit message for this, which this repository
    is too narrow to contain.

--- JavaScript.js
+++ JavaScript.js
@@ -7,60 +7,72 @@ var realHeight = 0;
 var zoom = 0;
 var image = null;
 var fileName = "";
-var annotations = null;
+var annotations = [];
 
+//Set up the page after the whole DOM is loaded.
 window.onload = function() {
-    //image = document.getElementById("hiddenImage");
-    window.addEventListener('resize', function() {
+    //Necessary so that the image doesn't stretch. Maybe add a waiting period here so that it doesn't fire too often.
+    window.addEventListener("resize", function() {
         renderImage();
     });
-    document.getElementById("mainCanvas").addEventListener("click", function (e) {
+    //Handle annotation viewing.
+    $("#mainCanvas").bind("click", function (e) {
+        //Refresh the image.
         renderImage();
-        var x = e.layerX;
-        var y = e.layerY;
+        //Click coordinates, relative to upper-left corner of the canvas.
+        var clickX = e.layerX;
+        var clickY = e.layerY;
+        //Do a hit-test for each annotation.
         for (var i = 0; i < annotations.length; i++) {
-            var aX = annotations[i].rlX;
-            var aY = annotations[i].rlY;
-            if ((x - aX) * (x - aX) + (y - aY) * (y - aY) <= 8 * 8 && annotations[i].rlX && annotations[i].rlY) {
-                var ctx = document.getElementById("mainCanvas").getContext('2d');
+            var annotationX = annotations[i].rlX;
+            var annotationY = annotations[i].rlY;
+            //If hotspot is currently on canvas (otherwise rlX == rlY == null) and the click is inside the circle:
+            if (annotations[i].rlX && annotations[i].rlY && (clickX - annotationX) * (clickX - annotationX) + (clickY - annotationY) * (clickY - annotationY) <= 8 * 8) {
+                var ctx = $("#mainCanvas")[0].getContext('2d');
                 ctx.font = "14px Arial";
-                
-                var w = ctx.measureText(annotations[i].note);
-                var bX = aX;
-                var bY = aY;
-                while (bX + w.width + 20 >= $("#mainCanvas").attr('scrollWidth')) {
-                    bX -= 15;
+                var textWidth = ctx.measureText(annotations[i].note);
+                //Set upper-left corner of annotation rectangle to the center of the circle.
+                var rectangleBaseX = annotationX;
+                var rectangleBaseY = annotationY;
+                //If the rectangle gets outside the screen, move it until it doesn't.
+                while (rectangleBaseX + textWidth.width + 20 >= $("#mainCanvas").attr("scrollWidth")) {
+                    rectangleBaseX -= 15;
                 }
-                while (bY + 40 >= $("#mainCanvas").attr('scrollHeight')) {
-                    bY -= 15;
+                while (rectangleBaseY + 40 >= $("#mainCanvas").attr("scrollHeight")) {
+                    rectangleBaseY -= 15;
                 }
+                //Draw the rectangle in white.
                 ctx.beginPath();
-                ctx.rect(bX, bY, w.width + 20, 40);
+                ctx.rect(rectangleBaseX, rectangleBaseY, textWidth.width + 20, 40);
                 ctx.fillStyle = "white";
                 ctx.strokeStyle = "black";
                 ctx.fill();
                 ctx.lineWidth = 2;
                 ctx.stroke();
+                //Draw the text.
                 ctx.fillStyle = "black";
-                ctx.fillText(annotations[i].note, bX + 10, bY + 25);
+                ctx.fillText(annotations[i].note, rectangleBaseX + 10, rectangleBaseY + 25);
             }
         }
     });
-    $("#brightnessrange").bind('change', function (e) {
-        var brightness = parseInt($(this).val());
-        setBrightness(brightness);
+    //Bind the events to sliders.
+    $("#brightnessrange").bind("change", function (e) {
+        setBrightness(parseInt($(this).val()));
     });
-    $("#contrastrange").bind('change', function (e) {
-        var contrast = parseInt($(this).val());
-        setContrast(contrast);
+    $("#contrastrange").bind("change", function (e) {
+        setContrast(parseInt($(this).val()));
     });
+    
     showList();
 };
 
 function setBrightness(b) {
     Caman("#hiddenImage", function () {
+        //Undo the previous changes.
         this.revert();
+        //Set relative brightness (b is <0; 100>, we need <-50; 50>).
         this.brightness(b - 50);
+        //When done, refresh the image.
         this.render(function () {
             renderImage();
         });
@@ -68,6 +80,7 @@ function setBrightness(b) {
 }
 
 function setContrast(c) {
+    //See setBrightness.
     Caman("#hiddenImage", function () {
         this.revert();
         this.contrast(c - 50);
@@ -78,12 +91,15 @@ function setContrast(c) {
 }
 
 function showList() {
+    //Grab the list of files from the server.
     $.ajax({
         url: "Default3.aspx",
         dataType: "json",
         success: function(data) {
             var fileArray = data.files;
+            //Wipe out the previous contents of the list (TODO necessary?)
             $("#linkList").empty();
+            //For each file, add a link to show it.
             for (var index = 0; index < fileArray.length; index++) {
                 $("#linkList").append("<a href=\"#\" onclick=\"fileName = \'" + fileArray[index].name + "\'; upLeftX = upLeftY = zoom = 0; loadPicture(); return false;\">" + fileArray[index].name + "</a>");
                 $("#linkList").append("<br \>");
@@ -94,27 +110,32 @@ function showList() {
 }
 
 function renderImage() {
-    document.getElementById('mainCanvas').getContext('2d').restore();
-    $("#mainCanvas").attr('width', $("#mainCanvas").attr('scrollWidth'));
-    $("#mainCanvas").attr('height', $("#mainCanvas").attr('scrollHeight'));
-    totalWidth = $('#hiddenImage').attr('width');
-    totalHeight = $('#hiddenImage').attr('height');
-    var ctx = document.getElementById('mainCanvas').getContext('2d');
-    ctx.drawImage(document.getElementById("hiddenImage"), upLeftX, upLeftY, $("#mainCanvas").attr('scrollWidth'), $("#mainCanvas").attr('scrollHeight'), 0, 0, $("#mainCanvas").attr('scrollWidth'), $("#mainCanvas").attr('scrollHeight'));
-    //add note drawing here
-    if (annotations == null) return;
+    $("#mainCanvas")[0].getContext('2d').restore();
+    //Set the width/height attributes of the canvas to actual width/height of the rendered element.
+    $("#mainCanvas").attr('width', $("#mainCanvas").attr("scrollWidth"));
+    $("#mainCanvas").attr('height', $("#mainCanvas").attr("scrollHeight"));
+    //Get the full image's dimensions.
+    totalWidth = $("#hiddenImage").attr("width");
+    totalHeight = $("#hiddenImage").attr("height");
+    var ctx = $("#mainCanvas")[0].getContext('2d');
+    //Clip the image to canvas dimensions and draw it on canvas.
+    ctx.drawImage($("#hiddenImage")[0], upLeftX, upLeftY, $("#mainCanvas").attr("scrollWidth"), $("#mainCanvas").attr("scrollHeight"), 0, 0, $("#mainCanvas").attr("scrollWidth"), $("#mainCanvas").attr("scrollHeight"));
+    //Draw the annotation hotspots. For each annotation:
     for (var i = 0; i < annotations.length; i++) {
+        //Calculate where it goes on the resized picture.
         var x = (totalWidth * annotations[i].x) / realWidth;
         var y = (totalHeight * annotations[i].y) / realHeight;
-        if (x >= upLeftX && y >= upLeftY && x < upLeftX + $("#mainCanvas").attr('scrollWidth') && y < upLeftY + $("#mainCanvas").attr('scrollHeight')) {
-            var rlX = x - upLeftX;
-            var rlY = y - upLeftY;
-            annotations[i].rlX = rlX;
-            annotations[i].rlY = rlY;
+        //If it's visible, then:
+        if (x >= upLeftX && y >= upLeftY && x < upLeftX + $("#mainCanvas").attr("scrollWidth") && y < upLeftY + $("#mainCanvas").attr("scrollHeight")) {
+            //Get the coordinates relative to the canvas' upper-left corner.
+            annotations[i].rlX = x - upLeftX;
+            annotations[i].rlY = y - upLeftY;
+            //Draw the circle.
             ctx.beginPath();
-            ctx.arc(rlX, rlY, 8, 0, 2 * Math.PI, false);
+            ctx.arc(annotations[i].rlX, annotations[i].rlY, 8, 0, 2 * Math.PI, false);
             ctx.fillStyle = "red";
             ctx.fill();
+        //If it's not visible, then mark it as such.
         } else {
             annotations[i].rlX = null;
             annotations[i].rlY = null;
@@ -123,10 +144,13 @@ function renderImage() {
     
 }
 function loadPicture() {
+    //Because fuck CamanJS, we need to remove the Caman-created canvas and add the actual img element.
     $('#hiddenImage').remove();
     $('#hiddenPictures').append("<img id='hiddenImage' />");
     $("#hiddenImage").error(function () { alert("Error!"); });
+    //Set the picture-fully-loaded handler.
     $('#hiddenImage').load(function () {
+        //Get real image width/height for annotations.
         $.ajax({
             url: "Default4.aspx?fileName=" + fileName,
             dataType: "json",
@@ -134,6 +158,7 @@ function loadPicture() {
             success: function (data) {
                 realWidth = data.x;
                 realHeight = data.y;
+                //Get the annotations.
                 $.ajax({
                     url: "Default5.aspx?fileName=" + fileName,
                     dataType: "json",
@@ -144,51 +169,46 @@ function loadPicture() {
                 })
             }
         });
+        //Re-set image parameters.
         setBrightness(parseInt($("#brightnessrange").val()));
         setContrast(parseInt($("#contrastrange").val()));
         renderImage();
     });
-    $('#hiddenImage').attr('src', "Default2.aspx?viewPortX=" + document.getElementById('mainCanvas').scrollWidth +
-        "&viewPortY=" + document.getElementById('mainCanvas').scrollHeight +
+    //Load the picture.
+    $('#hiddenImage').attr("src", "Default2.aspx?viewPortX=" + $("#mainCanvas")[0].scrollWidth +
+        "&viewPortY=" + $("#mainCanvas")[0].scrollHeight +
         "&zoomLevel=" + zoom +
         "&fileName=" + fileName);
-    /*Caman("#hiddenImage", "/Default2.aspx?viewPortX=" + document.getElementById('mainCanvas').scrollWidth +
-        "&viewPortY=" + document.getElementById('mainCanvas').scrollHeight +
-        "&zoomLevel=" + zoom +
-        "&fileName=" + fileName, function (e) {
-            this.brightness(0);
-            this.render(function() {
-                alert($("#hiddenImage").attr("width")); renderImage(); });
-        } );*/
-
 }
 function moveVPRight() {
-    if (upLeftX + document.getElementById('mainCanvas').scrollWidth + 40 <= totalWidth) {
+    if (upLeftX + $("#mainCanvas")[0].scrollWidth + 40 <= totalWidth) {
         upLeftX += 40;
         renderImage();
     }
-    else if (upLeftX + document.getElementById('mainCanvas').scrollWidth <= totalWidth) {
-        upLeftX = totalWidth - document.getElementById('mainCanvas').width;
+    else if (upLeftX + $("#mainCanvas")[0].scrollWidth <= totalWidth) {
+        upLeftX = totalWidth - $("#mainCanvas")[0].width;
         renderImage();
     }
 }
 function moveVPLeft() {
+    //Move by 40 pixels if possible,
     if (upLeftX - 40 >= 0) {
         upLeftX -= 40;
         renderImage();
     }
+    //otherwise move as far as you can.
     else if (upLeftX > 0) {
         upLeftX = 0;
         renderImage();
     }
 }
 function moveVPDown() {
-    if (upLeftY + document.getElementById('mainCanvas').scrollHeight + 40 <= totalHeight) {
+    if (upLeftY + $("#mainCanvas")[0].scrollHeight + 40 <= totalHeight) {
         upLeftY += 40;
         renderImage();
     }
-    else if (upLeftY + document.getElementById('mainCanvas').scrollHeight <= totalHeight) {
-        upLeftY = totalHeight - document.getElementById('mainCanvas').height;
+    else if (upLeftY + $("#mainCanvas")[0].scrollHeight <= totalHeight) {
+        upLeftY = totalHeight - $("#mainCanvas")[0].height;
         renderImage();
     }
 }
